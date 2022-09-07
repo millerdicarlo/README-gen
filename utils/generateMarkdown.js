2ea809b0849dfa8251abfb80c0d9d5d20e0f6216
millerd <millerdicarlo@gmail.com>
2022-09-07 17:22:03 -0400

    description and table of contents working

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -15,8 +15,15 @@ function generateMarkdown(data) {
   return `# ${data.title}
 
   ## Description
+    ${data.description}
 
   ## Table of Contents
+    * [Installation](#installation)
+    * [Usage](#usage)
+    * [License](#license)
+    * [Contributing](#contributing)
+    * [Tests](#tests)
+    * [Questions](#questions)
 
   ## Installation
 
